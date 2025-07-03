d3f2e857d4e25de65745a0078d0cc1bc4d49e076
Kevin Shu <shuster5494@gmail.com>
2025-07-03 17:44:48 -0400

    added dynamic environment configuration for plaid

--- main.go
+++ main.go
@@ -45,7 +45,11 @@ func init() {
 	configuration := plaid.NewConfiguration()
 	configuration.AddDefaultHeader("PLAID-CLIENT-ID", os.Getenv("PLAID_CLIENT_ID"))
 	configuration.AddDefaultHeader("PLAID-SECRET", os.Getenv("PLAID_SECRET"))
-	configuration.UseEnvironment(plaid.Sandbox) // Change to Development or Production as needed
+	if os.Getenv("ENV") != "production" {
+		configuration.UseEnvironment(plaid.Production)
+	} else {
+		configuration.UseEnvironment(plaid.Sandbox)
+	}
 	handlers.PlaidClient = plaid.NewAPIClient(configuration)
 
 	// Initialize Stripe client
